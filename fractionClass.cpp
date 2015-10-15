8167c780189800a2bbae2bfb39890f608a6568de
Harry Lewis <harry.lewis@queensu.ca>
2015-10-15 13:56:25 -0400

    Main files added and initialized

---
+++ fractionClass.cpp
@@ -0,0 +1,18 @@
+/*
+ * fractionClass.cpp
+ *
+ *      Author: Harry Lewis
+ *      Implementation for a Fraction Class using overloaded operators for CMPE320 assignment 1.
+ */
+
+// dependencies
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+// internal dependencies
+#include "insultGenerator.h"
+// current name space
+using namespace std;
---
+++ fractionClass.h
@@ -0,0 +1,14 @@
+/*
+ * fractionClass.h
+ *
+ *      Author: Harry Lewis
+ *      Definition for a Fraction Class using overloaded operators for CMPE320 assignment 2.
+ */
+
+// dependencies
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+// current namespace
+using namespace std;
